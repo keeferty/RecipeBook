4f6b0c89be5066f46b552e2968b069fadda70943
Pawel Weglewski <pawel.weglewski@getresponse.com>
2016-04-25 06:56:26 +0200

    DetailViewController semi-finished

---
+++ RecipeBook/Screens/RBDetailTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  RBDetailTableViewCell.swift
+//  RecipeBook
+//
+//  Created by Pawel Weglewski on 25/04/16.
+//  Copyright © 2016 Schibsted. All rights reserved.
+//
+
+import Cocoa
+
+class RBDetailTableViewCell: UITableViewCell {
+
+}
